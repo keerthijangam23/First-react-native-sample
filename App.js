f5f6b14085a3e4937890b7537a6086ff7cc6696a
keerthi <jangamkeerthi26@gmail.com>
2024-04-03 12:27:16 +0530

    Add practised core components

--- App.js
+++ App.js
@@ -1,20 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  ImageBackground,
+  ScrollView,
+  TextInput,
+  SafeAreaView,
+  KeyboardAvoidingView,
+} from "react-native";
+const logoImage = require("./assets/icon.png");
+import React, { useState } from "react";
+import Box from "./components/Box";
+import TouchableHighlightExample from "./components/TouchableHiglightExample";
+import TouchableOpacityExample from "./components/TouchableOpacity";
+import TouchableWithoutFeedbackExample from "./components/TouchableWithoutFeedback";
+import TouchableNativeFeedbackExample from "./components/TouchableNativeFeedbackExample";
+import LoginForm from "./components/LoginForm";
 
 export default function App() {
+  const [text, setText] = useState("");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.container}>
+      <Text>
+        <Text style={{ color: "white" }}>hello</Text> worldb
+      </Text>
+
+      <TextInput
+        placeholder="enter any text"
+        onChangeText={(text) => setText(text)}
+        style={{ height: 40 }}
+      />
+
+      <Image source={logoImage} style={{ width: 100, height: 100 }} />
+
+      <Image
+        source={{ uri: "https://reactnative.dev/docs/assets/p_cat2.png" }}
+        style={{ width: 100, height: 100 }}
+      />
+
+      <ImageBackground source={logoImage} style={{ flex: 1 }}>
+        <Text>Hello world</Text>
+      </ImageBackground>
+      
+
+      <TouchableHighlightExample />
+      <TouchableOpacityExample />
+      <TouchableWithoutFeedbackExample />
+      <TouchableNativeFeedbackExample />
+      <LoginForm />
+      <View>
+        <Box style={{ backgroundColor: "pink" }}>Box 1</Box>
+        <Box style={{ backgroundColor: "green" }}>Box 2</Box>
+        <Box style={{ backgroundColor: "yellow" }}>Box 3</Box>
+        <Box style={{ backgroundColor: "red" }}>Box 4</Box>
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "plum",
+    borderWidth: 6,
+    borderColor: "black",
   },
 });
---
+++ components/TouchableHiglightExample.js
@@ -0,0 +1,43 @@
+import React, {useState} from 'react';
+import {StyleSheet, Text, TouchableHighlight, View} from 'react-native';
+
+const TouchableHighlightExample = () => {
+  const [count, setCount] = useState(0);
+  const onPress = () => setCount(count + 1);
+
+  return (
+    <View style={styles.container}>
+      <TouchableHighlight onPress={onPress}>
+        <View style={styles.button}>
+          <Text>Touch Here</Text>
+        </View>
+      </TouchableHighlight>
+      <View style={styles.countContainer}>
+        <Text style={styles.countText}>{count}</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 10,
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+    margin:20
+  },
+  countContainer: {
+    alignItems: 'center',
+    padding: 10,
+  },
+  countText: {
+    color: '#FF00FF',
+  },
+});
+
+export default TouchableHighlightExample;
